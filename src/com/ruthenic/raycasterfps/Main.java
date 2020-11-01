99ede3bf89305b75b46656e2fe079b649e1df5fc
Ruthenic <mdrakea3@tutanota.com>
2020-11-01 04:27:43 -0500

    add nonfunctional level reading code

--- src/com/ruthenic/raycasterfps/Main.java
+++ src/com/ruthenic/raycasterfps/Main.java
@@ -1,8 +1,34 @@
 package com.ruthenic.raycasterfps;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+import java.io.FileWriter;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        Scanner s = null;
+        int i = 0;
+        int l = 0;
+        String username = System.getProperty("user.name");
+        try {
+            s = new Scanner(new File("/home/" + username + "/levels/level.txt"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        ArrayList<String>[][] level = new ArrayList[1024][1024];
+        while (s.hasNext()) {
+            level[i][l].add(s.next());
+            if (level[i][l].get(level[i][l].size() - 1) == "/") {
+                level[i][l].remove(level[i][l].get(level[i][l].size() - 1));
+                i = 0;
+                l++;
+            }
+        }
     }
 }
